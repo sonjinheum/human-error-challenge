2668721bf80d8db3320a7e709df73a4381d533f7
sonjinheum <son.jinheum@gmail.com>
2026-09-16 10:09:40 +0900

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
-// Firebase Console > 프로젝트 설정 > 내 앱 > SDK 설정 및 구성에서 복사해 아래 값을 교체하세요.
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://YOUR_DATABASE_URL",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.firebasestorage.app",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBTdQL109F-06x_yoKOkqEkQdPzKZJjBhY",
+  authDomain: "human-error-challenge.firebaseapp.com",
+  databaseURL: "https://human-error-challenge-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "human-error-challenge",
+  storageBucket: "human-error-challenge.firebasestorage.app",
+  messagingSenderId: "556888072803",
+  appId: "1:556888072803:web:9ee3015ef4349a45460e56",
+  measurementId: "G-BCFP7C8P7J"
 };
